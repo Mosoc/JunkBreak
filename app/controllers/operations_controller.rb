3c3a3b2ba75a925105363f87a4814c6c459d08f4
MOSOC <warshightide@gmail.com>
2015-11-10 15:03:49 +0000

    Finish Operation's CRUD and index page

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -1,5 +1,54 @@
 class OperationsController < ApplicationController
+  before_action :find_operation, only: [:show, :edit, :update, :destroy]
+  
   def index
-    flash[:notice] = "Servus /// Grüß dich"
+    #flash[:notice] = "Servus /// Grüß dich"
+    @operations = Operation.all;
   end
+  
+  def show
+    
+  end
+
+  def new
+    @operation = Operation.new
+  end
+  
+  def create
+    @operation = Operation.create(operation_params)
+
+    if @operation.save
+      redirect_to operations_path
+    else
+      render :new
+    end
+  end
+ 
+  def edit
+    
+  end
+ 
+  def update
+    if @operation.update(operation_params)
+      redirect_to operations_path, notice: "Refresh"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @operation.destroy
+    redirect_to operations_path, alert: "Deletion"
+  end
+  
+  private
+  
+  def operation_params
+    params.require(:operation).permit(:title, :description)
+  end
+  
+  def find_operation
+    @operation = Operation.find(params[:id])
+  end
+  
 end
